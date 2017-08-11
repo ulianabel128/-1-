13417c9488de362f57a3b887041daa0cc74a95cb
user <user@LAPTOP-M9GFNJT2>
2017-08-11 15:46:31 +0500

    Добавьте файлы проекта.

---
+++ задание 1 практика/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_1_практика
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
