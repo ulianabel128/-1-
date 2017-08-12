93eabbb8b39043d0d2206b027836cd7759bb74ba
Белкина Ульяна <uvbelkina@edu.hse.ru>
2017-08-12 15:21:57 +0500

    Я вспомнила, как работать с файлами

--- задание 1 практика/Program.cs
+++ задание 1 практика/Program.cs
@@ -13,45 +13,63 @@ namespace задание_1_практика
         {
             int[,] mas = new int [100, 100];
             int igrok = 1;
-            int x;
-            int y;
-            Random rand = new Random();
+            string x="";
+            string y="";
+            bool ok = true;
             FileStream file1 = new FileStream("INPUT.TXT", FileMode.Open); //создаем файловый поток 
+            StreamReader reader = new StreamReader(file1); // создаем «потоковый читатель» и связываем его с файловым потоком
 
-            x = rand.Next(0, 100);
-            y = rand.Next(0, 100);
+            string numbers = reader.ReadLine();
+            char[] ch_numbers = numbers.ToCharArray();
+            
 
-            Console.WriteLine("Координаты начальной клетки " + (x + 1) + " и " + (y + 1));
-
-            while ((x==0) || (y==0))
+            for (int i=0; i<ch_numbers.Length; i++)
             {
-                if ((x!=0) & (y!=0))
+                if (ok)
                 {
-                    x = x - 1;
-                    y = y - 1;
+                    x = x + ch_numbers[i];
+                    if (ch_numbers[i] != ' ') ok = false;
+                }
+                else
+                {
+                    y = y + ch_numbers[i];
+                }
+            }
+
+            int chislo_x;
+            int.TryParse(x, out chislo_x);
+
+            int chislo_y;
+            int.TryParse(y, out chislo_y);
+
+            Console.WriteLine("Координаты начальной клетки " + x + " и " + y);
+
+            chislo_x = -1;
+            chislo_y = -1;
+
+            while ((chislo_x==0) || (chislo_y==0))
+            {
+                if ((chislo_x!=0) & (chislo_y!=0))
+                {
+                    chislo_x = chislo_x - 1;
+                    chislo_y = chislo_y - 1;
                     if (igrok == 1) igrok = 2;
                     else igrok = 1;
                 }
             }
 
-            while (x==0)
+            while (chislo_y!=0)
             {
-                if (y!=0)
-                {
-                    y = y - 1;
+                    chislo_y = chislo_y - 1;
                     if (igrok == 1) igrok = 2;
                     else igrok = 1;
-                }
             }
 
-            while (y == 0)
+            while (chislo_x!= 0)
             {
-                if (x != 0)
-                {
-                    x = x - 1;
+                    chislo_x= chislo_x - 1;
                     if (igrok == 1) igrok = 2;
                     else igrok = 1;
-                }
             }
 
             Console.WriteLine(igrok);
